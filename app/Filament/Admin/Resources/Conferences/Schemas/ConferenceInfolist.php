1c7b450131258924a85ebf839f2901637fae7e4b
Marco "Mint" Introini <mint.dev@pm.me>
2025-10-21 16:59:44 +0200

    Conference View Resource

--- app/Filament/Admin/Resources/Conferences/Schemas/ConferenceInfolist.php
+++ app/Filament/Admin/Resources/Conferences/Schemas/ConferenceInfolist.php
@@ -2,7 +2,9 @@
 
 namespace App\Filament\Admin\Resources\Conferences\Schemas;
 
+use Filament\Infolists\Components\ImageEntry;
 use Filament\Infolists\Components\TextEntry;
+use Filament\Schemas\Components\Section;
 use Filament\Schemas\Schema;
 
 class ConferenceInfolist
@@ -11,13 +13,32 @@ class ConferenceInfolist
     {
         return $schema
             ->components([
-                TextEntry::make('name'),
+                TextEntry::make('user.name')
+                    ->label('Inserita da'),
+                TextEntry::make('name')
+                    ->label('Nome conferenza'),
+                Section::make('Informazioni sul luogo')
+                    ->columnSpanFull()
+                    ->columns()
+                    ->schema([
+                        TextEntry::make('location')
+                            ->label('Luogo'),
+                        TextEntry::make('comune.name')
+                            ->label('Comune'),
+                    ]),
                 TextEntry::make('description')
                     ->columnSpanFull(),
-                TextEntry::make('user.name')
-                    ->label('User'),
-                TextEntry::make('comune.name')
-                    ->label('Comune'),
+                Section::make('Date Conferenza')
+                    ->schema([
+                        TextEntry::make('start_date')
+                            ->dateTime('d/m/Y H:i')
+                            ->label('Data inizio'),
+                        TextEntry::make('end_date')
+                            ->dateTime('d/m/Y H:i')
+                            ->label('Data fine'),
+                    ]),
+                ImageEntry::make('cover_image'),
+                TextEntry::make('url'),
                 TextEntry::make('created_at')
                     ->dateTime()
                     ->placeholder('-'),
