4bdec6b27ae9e11fb447d00391a1f68e2c22d30e
Marco "Mint" Introini <mint.dev@pm.me>
2025-10-21 17:05:51 +0200

    insert user id

--- app/Filament/Admin/Resources/Conferences/Pages/CreateConference.php
+++ app/Filament/Admin/Resources/Conferences/Pages/CreateConference.php
@@ -8,4 +8,11 @@ use Filament\Resources\Pages\CreateRecord;
 class CreateConference extends CreateRecord
 {
     protected static string $resource = ConferenceResource::class;
+
+    protected function mutateFormDataBeforeCreate(array $data): array
+    {
+        $data['user_id'] = auth()->id();
+
+        return $data;
+    }
 }
