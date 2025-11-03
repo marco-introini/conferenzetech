3d38f5ee060e814daa629d15f0bfd2b4e1402131
Marco "Mint" Introini <mint.dev@pm.me>
2025-11-03 10:24:59 +0100

    add imageUrl accessor and casts for Conference model

--- app/Models/Conference.php
+++ app/Models/Conference.php
@@ -2,16 +2,33 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Support\Facades\Storage;
 
 class Conference extends Model
 {
     use HasFactory;
 
     protected $guarded = [];
+
+    protected function imageUrl(): Attribute
+    {
+        return Attribute::make(
+            get: fn ($value) => Storage::disk('conferences')->url($this->cover_image),
+        );
+    }
+
+    protected function casts(): array
+    {
+        return [
+            'start_date' => 'datetime',
+            'end_date' => 'datetime',
+        ];
+    }
 
     public function owner(): BelongsTo
     {
